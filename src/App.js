e853122e37831b4e457b11024c9f245d7f83f279
koebe1 <koe.be.pay@web.de>
2021-11-09 11:44:01 +0100

    text area

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [textarea, setTextarea] = useState("");
 
   const handleEmail = e => {
     console.log(e.target);
@@ -12,28 +13,42 @@ function App() {
   const handlePassword = e => {
     setPassword(e.target.value);
   };
+  const handleTextarea = e => {
+    setTextarea(e.target.value);
+  };
 
   return (
     <div className="App">
-      <form>
-        <input
-          type="email"
-          name="email"
-          value={email}
-          placeholder="email"
-          onChange={handleEmail}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          value={password}
-          placeholder="password"
-          onChange={handlePassword}
-          required
-        />
-        <button type="submit">submit</button>
-      </form>
+      <div className="form">
+        <form>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            placeholder="email"
+            onChange={handleEmail}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="password"
+            onChange={handlePassword}
+            required
+          />
+          <button type="submit">submit</button>
+        </form>
+      </div>
+      <div style={{ marginTop: "3vh" }} className="tex-area">
+        <textarea
+          name="text-area"
+          value={textarea}
+          placeholder="Enter text here..."
+          onChange={handleTextarea}
+        ></textarea>
+      </div>
+      <div style={{ marginTop: "3vh" }} className="select"></div>
     </div>
   );
 }
